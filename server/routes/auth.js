f0b54f278c98d5afeaa63ec59f640aab07c5c453
debapriyo43 <debapriyo43@gmail.com>
2023-07-08 16:00:49 +0530

    The signin api call- Made the connection with the DB using email and password which sent as body for the API call. And has received a token and user object where used ... operator to destructure the object

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
 
 // import { findOne } from 'mongoose'; // This help us with the mongoDB functions....
@@ -33,5 +34,25 @@ authRouter.post("/api/signup", async (req, res) => {
   //post the dareturn ta in database
   //return the data to the user
 });
+
+//signin Route
+authRouter.post('/api/signin',async (req,res)=>{
+  try{
+    const {email,password}=req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({msg:"User with this email does not exist"});
+    }
+    const isMatch = await bcryptjs.compare(password,user.password);
+    if(!isMatch){
+      return res.status(400).json({msg:"Incorrect password"});
+    }
+    const token = jwt.sign({id:user.id},"passwordKey");
+    res.json({token,...user._doc});
+  }catch(e){
+    res.status(500).json({error:e.message});
+  }
+})
+
 // With the below mentioned code we are mentioning that authRouter is not only a private Api it's now can be used by publicaly
 module.exports = authRouter;
