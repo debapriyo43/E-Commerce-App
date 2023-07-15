b3ccc016aae6afc3f3228ca81b405f3f684267e2
debapriyo43 <debapriyo43@gmail.com>
2023-07-15 16:53:11 +0530

    The bottons are implemented with the outlined button alos the upper section of the account is screen is ready.

--- lib/features/auth/account_screen.dart
+++ lib/features/auth/account_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../../constants/global_variables.dart';
+import '../account/widgets/below_app_bar.dart';
+import '../account/widgets/orders.dart';
+import '../account/widgets/top_buttons.dart';
 
 class AccountScreen extends StatelessWidget {
   const AccountScreen({super.key});
@@ -16,20 +19,37 @@ class AccountScreen extends StatelessWidget {
               gradient: GlobalVariables.appBarGradient,
             ),
           ),
-          title: Row(children: [
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
             Container(
               alignment: Alignment.topLeft,
               child: Image.asset('assets/images/shopnex.png'),
               width: 120,
-              height: 45,
+              height: 30,
               // color: Colors.black,
             ),
             Container(
-              
+              padding: const EdgeInsets.only(left:15,right:15),
+              child:Row(
+                children: const [
+                  Padding(padding: EdgeInsets.only(right:15),child: Icon(Icons.notifications_outlined),),
+                  Icon(Icons.search),
+                ],
+              )
             ),
 
           ]),
         ),
+      ),
+      body: Column(
+        children: const [
+          BelowAppBar(),
+          SizedBox(height:10),
+          TopButtons(),
+          SizedBox(height:20,),
+          Orders(),
+        ],
       ),
     );
   }
