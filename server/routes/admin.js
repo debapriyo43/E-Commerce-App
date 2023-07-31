e0009ccf1c92fac8ef83b4442913b24bd10937b3
debapriyo43 <debapriyo43@gmail.com>
2023-07-31 19:48:46 +0530

    The products showed at the admin screen using api

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -22,4 +22,14 @@ adminRouter.post("/admin/add-product",admin,async(req,res)=>{
         res.status(500).json({error:err.message});
     }
 });
+//Get all your products 
+// /admin/get-products
+adminRouter.get('/admin/get-products',admin,async(req,res)=>{
+    try{
+        const products= await Product.find({});
+        res.json(products);
+    }catch(e){
+        res.status(500).json({error:e.message});
+    }
+});
 module.exports=adminRouter;
