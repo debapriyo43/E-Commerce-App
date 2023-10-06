46d52b33ecc5006b150a90d30b1751643441d57b
debapriyo43 <debapriyo43@gmail.com>
2023-10-06 20:11:08 +0530

    The address box function implemented:

--- lib/features/cart/screens/cart_screen.dart
+++ lib/features/cart/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/common/widgets/custom_button.dart';
 import 'package:e_commerce_app/constants/global_variables.dart';
+import 'package:e_commerce_app/features/address/screens/address_screen.dart';
 import 'package:e_commerce_app/features/cart/widgets/cart_product.dart';
 import 'package:e_commerce_app/features/cart/widgets/cart_subtotal.dart';
 import 'package:e_commerce_app/features/home/widgets/address_box.dart';
@@ -18,6 +19,13 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   void navigateToSearchScreen(String query) {
     Navigator.pushNamed(context, SearchScreen.routeName, arguments: query);
+  }
+
+  void navigateToAddress() {
+    Navigator.pushNamed(
+      context,
+      AddressScreen.routeName,
+    );
   }
 
   @override
@@ -105,7 +113,7 @@ class _CartScreenState extends State<CartScreen> {
               padding: const EdgeInsets.all(8),
               child: CustomButton(
                 text: 'Proceed to buy (${user.cart.length} items)',
-                onTap: () {},
+                onTap: navigateToAddress,
                 color: Colors.yellow[600],
               ),
             ),
--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/features/address/screens/address_screen.dart';
 import 'package:e_commerce_app/features/admin/screens/add_product_screen.dart';
 import 'package:e_commerce_app/features/auth/screens/auth_Screen.dart';
 import 'package:e_commerce_app/features/home/screens/category_deals_screen.dart';
@@ -9,26 +10,49 @@ import 'package:flutter/material.dart';
 
 import 'common/widgets/bottom_bar.dart';
 
-Route<dynamic>genarateRoute(RouteSettings routeSettings){
-  switch(routeSettings.name){
+Route<dynamic> genarateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
     case AuthScreen.routeName:
-    return MaterialPageRoute(settings: routeSettings,builder: (_)=>const AuthScreen());
+      return MaterialPageRoute(
+          settings: routeSettings, builder: (_) => const AuthScreen());
     case HomeScreen.routeName:
-    return MaterialPageRoute(settings: routeSettings,builder: (_)=>const HomeScreen());
+      return MaterialPageRoute(
+          settings: routeSettings, builder: (_) => const HomeScreen());
     case BottomBar.routeName:
-    return MaterialPageRoute(settings: routeSettings,builder: (_)=>const BottomBar());
+      return MaterialPageRoute(
+          settings: routeSettings, builder: (_) => const BottomBar());
     case AddProductScreen.routeName:
-    return MaterialPageRoute(settings: routeSettings,builder: (_)=>const AddProductScreen());
+      return MaterialPageRoute(
+          settings: routeSettings, builder: (_) => const AddProductScreen());
     case CategoryDealsScreen.routeName:
-    var category=routeSettings.arguments as String;
-    return MaterialPageRoute(settings: routeSettings,builder: (_)=> CategoryDealsScreen(category: category,));
+      var category = routeSettings.arguments as String;
+      return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => CategoryDealsScreen(
+                category: category,
+              ));
     case SearchScreen.routeName:
-    var searchQueary=routeSettings.arguments as String;
-    return MaterialPageRoute(settings: routeSettings,builder: (_)=>SearchScreen(searchQueary: searchQueary,));
+      var searchQueary = routeSettings.arguments as String;
+      return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => SearchScreen(
+                searchQueary: searchQueary,
+              ));
     case ProductDetailScreen.routeName:
-    var product=routeSettings.arguments as Product;
-    return MaterialPageRoute(settings: routeSettings,builder: (_)=>ProductDetailScreen(product: product,));
+      var product = routeSettings.arguments as Product;
+      return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => ProductDetailScreen(
+                product: product,
+              ));
+    case AddressScreen.routeName:
+      return MaterialPageRoute(
+          settings: routeSettings, builder: (_) => const AddressScreen());
     default:
-      return MaterialPageRoute(settings: routeSettings,builder: (_)=>const Scaffold(body: Text('Something is going wrong'),));
+      return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => const Scaffold(
+                body: Text('Something is going wrong'),
+              ));
   }
 }
